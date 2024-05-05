a58d517e0ad923be94fd71ea51535b90fb6dce0b
J-Skarbek <john@snarkmediaagency.com>
2024-05-05 13:36:37 -0500

    Update header element

--- components/header.js
+++ components/header.js
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function MainHeader() {
   return (
-    <div>
+    <header>
       <ul>
         <li><Link href='/' >Home</Link></li>
         <li><Link href='/news' >News</Link></li>
       </ul>     
-    </div>
+    </header>
   )
 }

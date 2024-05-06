a89395522d0ecae74ed797df72f546799b9e4862
J-Skarbek <john@snarkmediaagency.com>
2024-05-06 11:51:45 -0500

    Intro to parrallel routes

---
+++ app/archive/@archive/page.js
@@ -0,0 +1,5 @@
+export default function ArchivePage() {
+  return (
+    <h1>Temporary Archive Page...</h1>
+  )
+}
---
+++ app/archive/@latest/page.js
@@ -0,0 +1,5 @@
+export default function LatestNewsPage() {
+  return (
+    <h1>Temporary Latest News Page...</h1>
+  )
+}

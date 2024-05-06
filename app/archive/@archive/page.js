10c0270be232882c755ad1e89d741addae318179
J-Skarbek <john@snarkmediaagency.com>
2024-05-06 16:00:06 -0500

    Refactor the mapping function for news lists into its own file

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -1,11 +1,16 @@
+import Link from "next/link";
+import { getAvailableNewsYears } from "@/lib/news"
+
 export default function ArchivePage() {
+  const links = getAvailableNewsYears();
+
   return (
     <header id="archive-header">
       <nav>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+          {links.map(link => <li key={link}>
+            <Link href={`/archive/${link}`}>{link}</Link>
+          </li>)}
         </ul>
       </nav>
     </header>
--- app/news/page.js
+++ app/news/page.js
@@ -1,23 +1,12 @@
-import Link from "next/link";
-
 import { DUMMY_NEWS } from "@/dummy-news";
+import NewsList from "@/components/newsList";
 
 export default function NewsPage() {
   return (
     <div id="news">
       <h1>This is the news page.</h1>
       <div>
-        <ul className="news-list">
-          { DUMMY_NEWS.map(newsItem => (
-            <li key={newsItem.id}>
-              <Link href={`/news/${newsItem.slug}`}>
-                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-                <span>{newsItem.title}</span>
-              </Link>
-            </li>
-          )
-          )}
-        </ul>
+        <NewsList news={DUMMY_NEWS} />
       </div>
     </div>
   );

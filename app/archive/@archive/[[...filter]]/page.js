496e6114a932e3b91cf099c96cc1ffb7c0108245
J-Skarbek <john@snarkmediaagency.com>
2024-05-08 15:02:30 -0500

    Add filter to filter and show news articles by their respective year

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -6,15 +6,33 @@ export default function FilteredNewsPage({ params }) {
   const filter = params.filter;
   const links = getAvailableNewsYears();
 
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map(link => <li key={link}>
-            <Link href={`/archive/${link}`}>{link}</Link>
-          </li>)}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map(link => <li key={link}>
+              <Link href={`/archive/${link}`}>{link}</Link>
+            </li>)}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   )
 }

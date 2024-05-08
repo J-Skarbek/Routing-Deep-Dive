87daef618336627b1aca1dc0c83457af1ebbd6bd
J-Skarbek <john@snarkmediaagency.com>
2024-05-08 15:34:55 -0500

    Add new error whenever an invalide filter is sent to url

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -27,6 +27,14 @@ export default function FilteredNewsPage({ params }) {
     newsContent = <NewsList news={news} />;
   }
 
+  //NOTE: the '+selectedYear/+selectedMonth uses the unary + operator, which
+  //can be used to convert a variable to a number Type (https://www.w3schools.com/js/js_type_conversion.asp)
+  if ((selectedYear && !getAvailableNewsYears().includes(+selectedYear)) || 
+    (selectedMonth && !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error('Invalid Filter.')
+  }
+
   return (
     <>
       <header id="archive-header">

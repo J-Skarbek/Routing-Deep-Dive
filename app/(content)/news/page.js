504aae6ec36bbb90b9f466629be6128bbe926c80
J-Skarbek <john@snarkmediaagency.com>
2024-05-09 14:16:10 -0500

    Refactor data fetching again to show a way to get data if you own the data source and can store your data directly on your server

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,19 +1,24 @@
 import NewsList from "@/components/newsList";
+import { getAllNews } from "@/lib/news";
 
 //By using Server Side Components, you can make the whole component async in nature
 //as seen below. Then in the function, you can directly await the response and handle
 //any errors -- additional Note: fetch is available and extended by nextJS to include
 //some special functionality around its use and how data it fetches gets cached
 export default async function NewsPage() {
-  const response = await fetch('http://localhost:8080/news');
+  // const response = await fetch('http://localhost:8080/news');
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch news.');
-  }
+  // if (!response.ok) {
+  //   throw new Error('Failed to fetch news.');
+  // }
 
-  const news = await response.json();
-  let newsContent;
-  
+  // const news = await response.json();
+
+  //ABOVE IS ALL CALLING DATA VIA A SEPERATE BACKEND/DB LIKE SET UP EARLIER, BELOW
+  //IS DOING THE SAME BUT PLACING THE DB FILE DIRECTLY IN THE NEXT APP
+
+  const news = getAllNews();
+   
   return (
     <div id="news">
       <h1>This is the news page.</h1>

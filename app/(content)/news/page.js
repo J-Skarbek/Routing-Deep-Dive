9d365497ebbe3b52a7a81994398e05526f8507c9
J-Skarbek <john@snarkmediaagency.com>
2024-05-09 14:07:13 -0500

    Refactor news fetching function to use server components

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,58 +1,24 @@
-'use client';
-
-import { useEffect, useState } from "react";
-// import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/newsList";
 
-export default function NewsPage() {
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState();
-  const [news, setNews] =  useState();
+//By using Server Side Components, you can make the whole component async in nature
+//as seen below. Then in the function, you can directly await the response and handle
+//any errors -- additional Note: fetch is available and extended by nextJS to include
+//some special functionality around its use and how data it fetches gets cached
+export default async function NewsPage() {
+  const response = await fetch('http://localhost:8080/news');
 
-  useEffect(() => {
-    //NOTE: apparently it is best practice to not make the useEffect hook itself
-    //async, but rather define a function within it and make it async instead
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:8080/news');
-
-      if (!response.ok) {
-        setError('Failed to fetch news.');
-        setIsLoading(false);
-      }
-
-      const news = await response.json();
-
-      setIsLoading(false);
-      setNews(news);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return (
-      <p>Loading...</p>
-    )
+  if (!response.ok) {
+    throw new Error('Failed to fetch news.');
   }
 
-  if (error) {
-    return (
-      <p>{error}</p>
-    )
-  }
-
+  const news = await response.json();
   let newsContent;
-
-  if (news) {
-    newsContent = <NewsList news={news} />
-  }
-
+  
   return (
     <div id="news">
       <h1>This is the news page.</h1>
       <div>
-        {newsContent}
+        <NewsList news={news} />
       </div>
     </div>
   );

4bad8d1f34822bea3f542fc431ff1eb3b2c9b556
J-Skarbek <john@snarkmediaagency.com>
2024-05-09 15:29:10 -0500

    More Refactoring to make some of the archvies use more suspense componnents

--- app/(content)/archive/@archive/[[...filter]]/page.js
+++ app/(content)/archive/@archive/[[...filter]]/page.js
@@ -3,6 +3,47 @@ import NewsList from "@/components/newsList";
 import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
 import { Suspense } from "react";
 
+//This and the FilteredNews async functions could in theory be put into their own files
+//But, since they are helper functions directly replated to the default export function
+//This tutorial is leaving them here for simplicitiy
+async function FilterHeader({ year, month }) {
+  const availableYears =  await getAvailableNewsYears();
+  let links = availableYears;
+
+  //NOTE: the '+selectedYear/+selectedMonth uses the unary + operator, which
+  //can be used to convert a variable to a number Type (https://www.w3schools.com/js/js_type_conversion.asp)
+  if ((year && !availableYears.includes(year)) || 
+    (month && !getAvailableNewsMonths(year).includes(month))
+  ) {
+    throw new Error('Invalid Filter.')
+  }
+
+  if (year && !month) {
+    links = getAvailableNewsMonths(year);
+  }
+
+  if (year && month) {
+    links = [];
+  }
+
+  return (
+    <header id="archive-header">
+    <nav>
+      <ul>
+        {links.map(link => {
+          const href = year ? `/archive/${year}/${link}` : `archive/${link}`;
+          return (
+            <li key={link}>
+              <Link href={href}>{link}</Link>
+            </li>
+          );  
+        })}
+      </ul>
+    </nav>
+  </header>
+  )
+}
+
 async function FilteredNews({ year, month }) {
   let news;
   if (year && !month) {
@@ -26,41 +67,11 @@ export default async function FilteredNewsPage({ params }) {
   const selectedYear = filter?.[0];
   const selectedMonth = filter?.[1];
 
-  const availableYears =  await getAvailableNewsYears();
-  let links = availableYears;
-
-  if (selectedYear && !selectedMonth) {
-    links = getAvailableNewsMonths(selectedYear);
-  }
-
-  if (selectedYear && selectedMonth) {
-    links = [];
-  }
-
-  //NOTE: the '+selectedYear/+selectedMonth uses the unary + operator, which
-  //can be used to convert a variable to a number Type (https://www.w3schools.com/js/js_type_conversion.asp)
-  if ((selectedYear && !availableYears.includes(selectedYear)) || 
-    (selectedMonth && !getAvailableNewsMonths(selectedYear).includes(selectedMonth))
-  ) {
-    throw new Error('Invalid Filter.')
-  }
-
   return (
     <>
-      <header id="archive-header">
-        <nav>
-          <ul>
-            {links.map(link => {
-              const href = selectedYear ? `/archive/${selectedYear}/${link}` : `archive/${link}`;
-              return (
-                <li key={link}>
-                  <Link href={href}>{link}</Link>
-                </li>
-              );  
-            })}
-          </ul>
-        </nav>
-      </header>
+      <Suspense fallback={<p>Loading month/year filter...</p>}>
+        <FilterHeader year={selectedYear} month={selectedMonth} />
+      </Suspense>
       <Suspense fallback={<p>Loading News Via Suspense Component...</p>}>
         <FilteredNews year={selectedYear} month={selectedMonth} />
       </Suspense>

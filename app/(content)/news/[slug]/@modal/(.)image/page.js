d8c3f87e21598eaf88ad359b8ff3e95d47ced976
J-Skarbek <john@snarkmediaagency.com>
2024-05-09 14:45:27 -0500

    Refactor modal backdrop into its own component

--- app/(content)/news/[slug]/@modal/(.)image/page.js
+++ app/(content)/news/[slug]/@modal/(.)image/page.js
@@ -1,13 +1,11 @@
-'use client';
-
-import { notFound, useRouter } from "next/navigation";
+import { notFound } from "next/navigation";
 import { DUMMY_NEWS } from "@/dummy-news";
+import ModalBackdrop from "@/components/modalBackdrop";
 
 //NOTE: this intercepted path will be navigated to by people who are trying to visit the path
 //While viewing another existing component in the app:
 //https://nextjs.org/docs/app/building-your-application/routing/intercepting-routes
 export default function InterceptedImagePage({ params }) {
-  const router = useRouter();
 
   const newsItemSlug = params.slug;
 
@@ -20,7 +18,7 @@ export default function InterceptedImagePage({ params }) {
   return (
     <>
       {/* <h1>INTERCEPTED!</h1> */}
-      <div className="modal-backdrop" onClick={router.back} />
+      <ModalBackdrop />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} width='350' />
